2336f065b8126bdd547c5a663e6375e70ccd5b18
Vidyasekaran <vidguru1008@gmail.com>
2023-01-09 14:09:50 +0530

    first commit

---
+++ String.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+mylist=['apple','orange']
+len(mylist)
+
+
+# In[4]:
+
+
+mylist=['apple',100,10.3]
+mylist[0]
+
+
+# In[7]:
+
+
+mylist1=[500]
+newlist=mylist+mylist1
+newlist
+
+
+# In[ ]:
+
+
+
+
